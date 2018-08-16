ce511089e79b44617c4e02c83bccc4696e9cbd25
CatchTheDog <1924528451@qq.com>
2018-08-16 16:02:45 +0800

    类型信息

--- src/com/thinkinjava/example/typeinfo/ClassInitialization.java
+++ src/com/thinkinjava/example/typeinfo/ClassInitialization.java
@@ -2,6 +2,9 @@ package com.thinkinjava.example.typeinfo;
 
 import java.util.Random;
 
+/**
+ * 类型信息
+ */
 public class ClassInitialization {
     public static Random rand = new Random(47);
     public static void main(String [] args){
